af6f0a4048f4ddb61987ada60314aa9e669b1e34
Andrew Blowe <tehandyb@gmail.com>
2015-05-21 12:59:22 -0400

    fixed and simplified logic for showing the expand button, also added clear all

--- lib/react-tag-list.jsx
+++ lib/react-tag-list.jsx
@@ -7,6 +7,7 @@ module.exports = React.createClass({
   propTypes: {
     values: React.PropTypes.array,
     onRemove: React.PropTypes.func,
+    removeAll: React.PropTypes.func,
     tagHeight: React.PropTypes.number,
     collapsedRows: React.PropTypes.number,
     expandRows: React.PropTypes.number,
@@ -22,27 +23,35 @@ module.exports = React.createClass({
     return {expanded: false, showExpandButton: false, shownCount: 0, rows: 0};
   },
   componentDidUpdate: function() {
-    if(this.props.values.length === 0) return;
 
     //if a row was added, update the state
     if(this.getRows() !== this.state.rows) this.setState({rows: this.getRows()});
 
-    var lastTag, ltRef, isOverflowing;
-    ltRef = "tag-" + String(this.props.values.length-1);
-    lastTag = this.refs[ltRef].getDOMNode();
-    isOverflowing = this.isTagOverflowing(lastTag);
-
-    //show the expand button if it was previously not shown, and it should be overflowing
-    if(!this.state.showExpandButton && ((isOverflowing && !this.state.expanded) || this.state.expanded))
-      this.setState({showExpandButton: true, shownCount: this.getShownCount()});
-    //if the state is in showExpandButton mode, yet it is not expanded and is not overflowing
-    //this is probably because some tags were removed so stop showing the expand button
-    else if(this.state.showExpandButton && !isOverflowing && !this.state.expanded)
-      this.setState({showExpandButton: false, shownCount: this.getShownCount()});
-
     //if the shown count updated, update the state
     if(this.state.shownCount !== this.getShownCount())
       this.setState({shownCount: this.getShownCount()});
+
+    var lastTag, ltRef, isOverflowing;
+    //when there are no tags it is not overflowing
+    if(this.props.values.length === 0)
+      isOverflowing = false;
+    else {
+      ltRef = "tag-" + String(this.props.values.length-1);
+      lastTag = this.refs[ltRef].getDOMNode();
+      isOverflowing = this.isTagOverflowing(lastTag);
+    }
+
+    //check that tags are not overflowing over the collapsed height
+    //check that showExpandButton was previously true to avoid an infinite render loop
+    //then set showExpandButton to false since it is not needed to be shown
+    if(!isOverflowing && this.state.showExpandButton)
+      this.setState({showExpandButton: false, expanded: false});
+    //check that tags are overflowing
+    //check that showExpandButton was previously false to avoid infinite render loop
+    //then set showExpandButton to true so we can expand to show overflow
+    else if(isOverflowing && !this.state.showExpandButton)
+      this.setState({showExpandButton: true});
+
   },
   onRemoveFunc: function() {
     if(this.props.onRemove)
@@ -51,20 +60,30 @@ module.exports = React.createClass({
       return function(value) { return value;};
   },
   toggleExpand: function() {
-    this.setState({expanded: !this.state.expanded, shownCount: this.getShownCount()});
+      this.setState({expanded: !this.state.expanded, shownCount: this.getShownCount()});
   },
   isTagOverflowing: function(tagDOMNode) {
-    var containerBottom = this.getBottomOfElement(this.refs["rtl-tags"].getDOMNode());
+    if(this.props.values.length === 0) return false;
+    //over flow is determined if any tags are below the collapsed container 
+    //also overflow will be false if there are no tags
+    var collapsedBottom = this.getBottomOfElement(this.refs["tag-0"].getDOMNode()) * this.props.collapsedRows;
+
     //check that the top of the last tag is above or below the bottom of the container
-    if(this.getBottomOfElement(tagDOMNode) > containerBottom)
+    if(this.getBottomOfElement(tagDOMNode) > collapsedBottom)
       return true;
 
     return false;
   },
+  removeAllTags: function() {
+    
+    this.setState({showExpandButton: false}, this.props.removeAll);
+  },
   getShownCount: function() {
     //go through all values, get each ref element, 
-    //and check if the bottom of it is overlapped by the bottom of the tag list box
+    //and check if the bottom of it is overlapped by the bottom of the collapsed tag container 
     var shownCount = 0;
+    if(this.props.values.length === 0) return shownCount;
+
     this.props.values.forEach(function(v, vIndex) {
       var tagDOMNode = this.refs["tag-" + vIndex].getDOMNode();
       if(this.isTagOverflowing(tagDOMNode))
@@ -79,6 +98,8 @@ module.exports = React.createClass({
   getRows: function() {
     //get the tag for each value, add a row to the count when a new max bottom tag is found
     var rowCount = 0, currentMax = 0;
+    if(this.props.values.length === 0) return rowCount;
+
     this.props.values.forEach(function(v, vIndex) {
       if(!this.refs["tag-" + vIndex]) {
         // rowCount ++;
@@ -115,7 +136,8 @@ module.exports = React.createClass({
   }, 
   render: function() {
 
-    var tags, containerHeight, expandText, expandButton, parentCollapsedStyleName, collapsedStyleName, countText;
+    var tags, containerHeight, expandText, expandButton, parentCollapsedStyleName,
+      collapsedStyleName, countText, showMoreTitle, clearAllButton;
 
     tags = this.props.values.map(function(value, vIndex) {
       if(this.props.easyClick) {
@@ -151,11 +173,18 @@ module.exports = React.createClass({
     }
 
     if(this.state.showExpandButton) {
-      if(this.state.expanded) countText = "";
-      else countText = "+ " + this.props.values.length;
+      if(this.state.expanded) {
+        countText = "";
+        showMoreTitle = "Show Less";
+      }
+      else {
+        countText = "+ " + (this.props.values.length - this.state.shownCount);
+        showMoreTitle = "Show More";
+      }
+
       expandButton = (
         <li className="expand-control expand-control-show">
-          <button onClick={this.toggleExpand} className="expand-button" title={"+ " + this.props.values.length}>
+          <button onClick={this.toggleExpand} className="expand-button" title={showMoreTitle}>
           <div className="show-count">{countText}</div>
           <div className="expand-text">{expandText}</div>
           </button>
@@ -164,12 +193,10 @@ module.exports = React.createClass({
     } else {
       expandButton = (
         <li className="expand-control expand-control-hide">
-          <button onClick={this.toggleExpand} className="expand-button" title={"+ " + this.props.values.length}>
-            <div className="show-count">{String(this.state.shownCount) + " of " + this.props.values.length}</div>
-            <div className="expand-text">{expandText}</div>
-          </button>
         </li>);
     }
+
+    clearAllButton = <li className="clear-all-button" onClick={this.removeAllTags}>X</li>
 
     var rtlStyles = {
       maxHeight: containerHeight
@@ -179,6 +206,7 @@ module.exports = React.createClass({
       <div ref="rtl-container" className={"react-tag-list" + " " + parentCollapsedStyleName}>
         <ul ref="rtl-tags" className={"rtl-tags" + " " + collapsedStyleName} style={rtlStyles}>
           {tags} 
+          {clearAllButton}
           {expandButton}
         </ul>
       </div>
