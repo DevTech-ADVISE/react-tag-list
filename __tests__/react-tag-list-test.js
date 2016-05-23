30974a056076cc5f23fc40f42463e5dc59394ba6
Jack Compton <jack.compton@gmail.com>
2016-05-23 15:38:30 -0400

    use Jest, fix tests, node to 6.2.0, react to 15.1.0

--- __tests__/react-tag-list-test.js
+++ __tests__/react-tag-list-test.js
@@ -1,20 +1,24 @@
+jest.unmock('../dist/react-tag-list.js');
+
 var React = require('react');
-var TestUtils = require('react-addons-test-utils');;
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
 var ReactTagList = require('../dist/react-tag-list');
 var testData = require('js-test-data');
 
 describe('ReactTagList', function() {
-  var component, onRemoveValue;
+  var component, onRemoveValue, domNode;
   var onRemove = function(tagValue) {onRemoveValue = tagValue;};
 
   beforeEach(function() {
     component = TestUtils.renderIntoDocument(
       <ReactTagList values={testData.Toppings} onRemove={onRemove}/>
     );
+    domNode = ReactDOM.findDOMNode(component);
   });
 
   it('should render', function() {
-    expect(component.getDOMNode().className).toMatch('react-tag-list');
+    expect(domNode).toBeDefined();
   });
 
   it('should render tags with the supplied values', function() {
@@ -22,15 +26,15 @@ describe('ReactTagList', function() {
     for(var i = 0; i < testData.Toppings.length; i++) {
       var refKey = 'tag-' + String(i);
       expect(component.refs[refKey]).toBeDefined();
-      expect(component.refs[refKey].getDOMNode().getElementsByClassName('rtl-label')[0].innerHTML)
+      expect(component.refs[refKey].getElementsByClassName('rtl-label')[0].innerHTML)
             .toEqual(testData.Toppings[i].label);
     }
   });
 
   it('should call onRemove function when the X button is clicked', function() {
-  	//click to remove the first tag
-  	TestUtils.Simulate.click(component.refs['tag-0-remove'].getDOMNode());
-  	//the onRemove function should be called, and set our onRemoveValue to what value is being removed
-  	expect(onRemoveValue).toEqual(testData.Toppings[0].value);
+    //click to remove the first tag
+    TestUtils.Simulate.click(component.refs['tag-0-remove']);
+    //the onRemove function should be called, and set our onRemoveValue to what value is being removed
+    expect(onRemoveValue).toEqual(testData.Toppings[0].value);
   });
 });
